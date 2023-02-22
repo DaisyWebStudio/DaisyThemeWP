3d1232276c83984cc6c2f3b60e5df9b2214387b5
Yulia2331 <86805193+Yulia2331@users.noreply.github.com>
2023-02-22 18:07:31 +0300

    Пример ui kit

--- wp-content/themes/daisy/inc/custom-fields/main-style.php
+++ wp-content/themes/daisy/inc/custom-fields/main-style.php
@@ -6,9 +6,15 @@ add_action( 'carbon_fields_register_fields', 'crb_main_style' );
 function crb_main_style() {
     Container::make('theme_options', 'Настройки ui kit')
     ->add_tab('Настройки шрифтов', array(
-        
+        Field::make( "select", "font_family", "Выбрать основной шрифт" )
+        ->add_options( array(
+            'roboto' => 'roboto',
+            'more'     => 'Другой пример шрифта',
+        ) )->set_width(50),
     ))
     ->add_tab('Настройки цветов', array(
-        
+        Field::make( "separator", "btn_primary", "Главная кнопка" ),
+        Field::make( "color", "btn_primary_bg_color", "Фоновый цвет" )->set_width(25),
+        Field::make( "color", "btn_primary_color", "Цвет текста" )->set_width(25),
     ));
 }
